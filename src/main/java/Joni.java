33e8bc25ee4611860d949935dc4e3daa8fdd55e1
AbiHalim <abishai.halim@gmail.com>
2025-01-21 19:39:22 +0800

    Added task list functionality

--- src/main/java/Joni.java
+++ src/main/java/Joni.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Joni {
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         String logo = "    .---.    .-'''-.                    \n" +
                 "    |   |   '   _    \\                  \n" +
@@ -25,18 +29,45 @@ public class Joni {
 
         Scanner sc = new Scanner(System.in);
         while (true) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
+
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                printTaskList();
+            } else {
+                addTask(input);
             }
+        }
+        sc.close();
+    }
+
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = task;
+            taskCount++;
             System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
+            System.out.println(" added: " + task);
+            System.out.println("____________________________________________________________");
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Sorry, I can't add more tasks. The list is full!");
             System.out.println("____________________________________________________________");
         }
+    }
 
-        sc.close();
+    private static void printTaskList() {
+        System.out.println("____________________________________________________________");
+        if (taskCount == 0) {
+            System.out.println(" No tasks added yet.");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            }
+        }
+        System.out.println("____________________________________________________________");
     }
 }
