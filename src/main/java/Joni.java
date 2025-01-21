9e06a9971ab10d1243197dd93c2dae2c7a43a55d
AbiHalim <abishai.halim@gmail.com>
2025-01-21 19:36:37 +0800

    Implemented Echo

--- src/main/java/Joni.java
+++ src/main/java/Joni.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Joni {
     public static void main(String[] args) {
         String logo = "    .---.    .-'''-.                    \n" +
@@ -13,14 +15,28 @@ public class Joni {
                 " __.'   '               |  |   |  |     \n" +
                 "|      '                |  |   |  |     \n" +
                 "|____.'                 '--'   '--'     \n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
 
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Joni");
+        System.out.println(" Hello! My name is Joni");
+        System.out.println(" And this is my promise: helping you!");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        sc.close();
     }
 }
