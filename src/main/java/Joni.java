3faec9e45dc082e7b32341f4dff462e2189948de
AbiHalim <abishai.halim@gmail.com>
2025-01-21 19:32:14 +0800

    Renamed Duke to Joni

---
+++ src/main/java/Joni.java
@@ -0,0 +1,26 @@
+public class Joni {
+    public static void main(String[] args) {
+        String logo = "    .---.    .-'''-.                    \n" +
+                "    |   |   '   _    \\                  \n" +
+                "    '---' /   /` '.   \\    _..._   .--. \n" +
+                "    .---..   |     \\  '  .'     '. |__| \n" +
+                "    |   ||   '      |  '.   .-.   ..--. \n" +
+                "    |   |\\    \\     / / |  '   '  ||  | \n" +
+                "    |   | `.   ` ..' /  |  |   |  ||  | \n" +
+                "    |   |    '-...-'`   |  |   |  ||  | \n" +
+                "    |   |               |  |   |  ||  | \n" +
+                "    |   |               |  |   |  ||__| \n" +
+                " __.'   '               |  |   |  |     \n" +
+                "|      '                |  |   |  |     \n" +
+                "|____.'                 '--'   '--'     \n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Joni");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
