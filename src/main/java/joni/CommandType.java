6918e57b129d3a9bb94ad24774fdd8751d565ea5
AbiHalim <abishai.halim@gmail.com>
2025-01-30 14:41:25 +0700

    Fix switch case indentation in CommandType.java

--- src/main/java/joni/CommandType.java
+++ src/main/java/joni/CommandType.java
@@ -5,15 +5,16 @@ enum CommandType {
 
     public static CommandType fromString(String command) {
         switch (command.toLowerCase()) {
-            case "bye": return BYE;
-            case "list": return LIST;
-            case "mark": return MARK;
-            case "unmark": return UNMARK;
-            case "todo": return TODO;
-            case "deadline": return DEADLINE;
-            case "event": return EVENT;
-            case "delete": return DELETE;
-            default: return UNKNOWN;
+        case "bye": return BYE;
+        case "exit": return BYE;
+        case "list": return LIST;
+        case "mark": return MARK;
+        case "unmark": return UNMARK;
+        case "todo": return TODO;
+        case "deadline": return DEADLINE;
+        case "event": return EVENT;
+        case "delete": return DELETE;
+        default: return UNKNOWN;
         }
     }
 }
